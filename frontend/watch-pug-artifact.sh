8a36306b07c7f19e81bed3e916ca9bdb7d6104fa
zhukovsd <zhukovsd@gmail.com>
2018-05-28 16:03:06 +0300

    Remove unnecessary files - old build and watch shell scripts

--- frontend/watch-pug-artifact.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# set current directory to ./frontend explicitly, because this script gets executed from a root project dir
-cd "${0%/*}"
-
-./node_modules/.bin/pug app -o ./../target/typing-test-1.0-SNAPSHOT/ -P --watch
--- frontend/watchify-artifact.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# set current directory to ./frontend explicitly, because this script gets executed from a root project dir
-cd "${0%/*}"
-
-watchify app/js/app.js -t [babelify] --debug -o ./../target/typing-test-1.0-SNAPSHOT/app.js -v
